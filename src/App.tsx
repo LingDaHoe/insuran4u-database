d42311aedae7261a1d0cab97cae4ddbcf822b338
LingDaHoe <eatapplepie7272@gmail.com>
2025-07-30 21:50:06 +0800

    fix: set BrowserRouter basename for GitHub Pages subpath

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/insuran4u-database/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/staff-login" element={<StaffLogin />} />
